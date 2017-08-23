e71bccf04d9d2400681b639a3e05a4a9991d7c5d
anuprshetty <anuprshetty@gmail.com>
2017-08-23 17:00:50 +0530

    fibonacci frontend dockerization

---
+++ fibonacci/frontend/docker_files/dev.Dockerfile
@@ -0,0 +1,19 @@
+FROM node:18-alpine
+
+ARG PROJECT_ROOT_FOLDER=frontend
+
+# set current working directory
+WORKDIR /developer/projects/$PROJECT_ROOT_FOLDER
+RUN echo "Current working directory: $(pwd)"
+
+RUN apk update && apk add bash
+
+COPY .. .
+
+RUN npm install
+
+EXPOSE 3000
+
+RUN chmod 755 ./scripts/dev.sh
+
+CMD ./scripts/dev.sh
---
+++ fibonacci/frontend/scripts/dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# “set -e” option causes the script to exit immediately if any command exits with a non-zero exit code.
+set -e
+
+echo "Current working directory: $(pwd)"
+
+chmod 755 ./scripts/wait-for-it.sh
+
+./scripts/wait-for-it.sh --host=backend --port=5000 --strict --timeout=0 -- echo 'backend is ready'
+
+npm run debug
